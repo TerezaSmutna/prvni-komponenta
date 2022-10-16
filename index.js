b0c557d52f52f21213a290b12130d96174fae4ca
Martin Podloucký <podlouckymartin@gmail.com>
2022-10-16 21:12:48 +0200

    Přizpůsobení aktuální výuce.

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 import { ShoppingItem } from "./ShoppingItem/index.js";
 
-const items = [
-  {
-    product: 'Rohlíky',
-    amount: '10',
-    done: true,
-  },
-  {
-    product: 'Rajčata',
-    amount: '1 kg',
-    done: false,
-  },
-  {
-    product: 'Máslo',
-    amount: '500 g',
-    done: false,
-  },
-];
+const renderShoppingList = (items) => {
+  const shoppingList = document.querySelector('.shopping-list');
+  shoppingList.innerHTML = items
+    .map((item) => ShoppingItem(item))
+    .join('');
+};
 
-const listElement = document.querySelector('.shopping-list');
-for (let i = 0; i < items.length; i += 1) {
-  listElement.innerHTML += ShoppingItem(items[i]);
-}
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/shopping')
+  .then((response) => response.json())
+  .then((data) => renderShoppingList(data));
