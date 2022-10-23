49274b2bb61e7bfcc44e27a58e620a01fbd66353
Martin Podloucký <podlouckymartin@gmail.com>
2022-10-23 21:08:44 +0200

    Vlastní stahování dat

--- ShoppingList/index.js
+++ ShoppingList/index.js
@@ -1,17 +1,27 @@
 import { ShoppingItem } from "../ShoppingItem/index.js";
 
 export const ShoppingList = (props) => {
-  const { day, items } = props;
-  
+  const { day, dayName, items = [] } = props;
+
   const element = document.createElement('div');
   element.classList.add('shopping-list');
   element.innerHTML = `
-    <h2>${day}</h2>  
+    <h2>${dayName}</h2>
     <ul class="shopping-list__items"></ul>
   `;
   
+  fetch(`https://apps.kodim.cz/daweb/trening-api/apis/shopping/${day}`)
+    .then((response) => response.json())
+    .then((data) => {
+      element.replaceWith(ShoppingList({
+        day: day,
+        dayName: dayName,
+        items: data,
+      }));
+    });
+
   const ulElement = element.querySelector('ul');
   ulElement.append(...items.map((item) => ShoppingItem(item)));
-  
+
   return element;
 };
--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 import { ShoppingList } from './ShoppingList/index.js';
 
-const listsElement = document.querySelector('#lists');
-
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/shopping/mon')
-  .then((response) => response.json())
-  .then((data) => {
-    listsElement.append(ShoppingList({ day: 'Pondělí', items: data }));
-  });
-
-fetch('https://apps.kodim.cz/daweb/trening-api/apis/shopping/tue')
-  .then((response) => response.json())
-  .then((data) => {
-    listsElement.append(ShoppingList({ day: 'Úterý', items: data }));
-  });
+document.querySelector('#lists').append(
+  ShoppingList({ day: 'mon', dayName: 'Pondělí' }),
+  ShoppingList({ day: 'tue', dayName: 'Úterý' }),
+);
