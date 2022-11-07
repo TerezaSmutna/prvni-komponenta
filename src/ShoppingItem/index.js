30bb96c6b44cc2402bc08ef3ed7b5dd4ebbdf02a
Martin Podloucký <podlouckymartin@gmail.com>
2022-11-07 22:04:33 +0100

    Styly pro tlačítko delete

--- src/ShoppingItem/index.js
+++ src/ShoppingItem/index.js
@@ -3,9 +3,9 @@ import './style.css';
 export const ShoppingItem = (props) => {
   const { id, product, amount, unit, done } = props;
 
-  let checkClass = '';
+  let checkClass = 'icon-nocheck';
   if (done) {
-    checkClass = 'item__btn-done--check';
+    checkClass = 'icon-check';
   }
 
   const element = document.createElement('li');
@@ -13,7 +13,8 @@ export const ShoppingItem = (props) => {
   element.innerHTML = `
     <div class="item__name">${product}</div>
     <div class="item__amount">${amount} ${unit}</div>
-    <button class="item__btn-done ${checkClass}"></button>
+    <button class="item__btn ${checkClass}"></button>
+    <button class="item__btn icon-cross"></button>
   `;
   element.querySelector('button').addEventListener('click', () => {
     fetch(`https://apps.kodim.cz/daweb/shoplist/api/weeks/0/days/mon/${id}`, {
